2c91e5dc9525d9ceca014f636ec2d4b00237c294
Henry Zhang <henryyihengzhang@gmail.com>
2018-01-11 16:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-function theBeatlesPlay(musicians, instruments) {
-  var arr = []
+function theBeatlesPlay(musicians, instruments){
 
-  for (var i = 0; i < musicians.length; i++) {
-    arr.push(`${musicians[i]} plays ${instruments[i]}`)
-  }
+var arr = [];
 
-  return arr
+for (var i = 0; i < musicians.length; i++) {
+  arr.push(`${musicians[i]} plays ${instruments[i]}`);
+}
+return arr;
 }
 
-function johnLennonFacts(facts) {
-
-  var shoutedFacts = []
-  let i = 0
-
-  while (i < facts.length) {
-
-    shoutedFacts.push(`${facts[i]}!!!`)
-    i++
-  }
-
-  return shoutedFacts
-}
-
-
-function iLoveTheBeatles(n) {
-  var arr = []
-
-  do {
-    arr.push("I love the Beatles!")
-    n++
-  }
-  while (n < 15)
-
-  return arr
-}
